83f0d2992f9a0bdf28796ed49d7e6569a164a438
namanica <veronichkapilipna@gmail.com>
2024-01-13 22:25:50 +0200

    proposed additional solution with array methods

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -5,6 +5,8 @@ const duplicate = (value, number) => {
   if (number <= 0) {
     return [];
   } else {
+    //second variant using array methods
+    //return [...Array(number).keys()].map(el=> value);
     const res = [];
     for (let i = 0; i < number; i++) {
       res[i] = value;
