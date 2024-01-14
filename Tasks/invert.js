31ca97046bbf84998068ae219ad742921960cd1a
namanica <veronichkapilipna@gmail.com>
2024-01-14 14:11:18 +0200

    deleted unused variables

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -2,14 +2,7 @@
 'use strict';
 
 const invertArr = (arr) => {
-  T = Object.keys(A, 4);
-  T.forEach((_, i) => {
-    T[i] = A.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
-  return T;
+  const reversedArr = [];
 };
 
 module.exports = invertArr;
