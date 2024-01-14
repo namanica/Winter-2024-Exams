b3e9a374b3b9a466a1d987055addc879538000b4
namanica <veronichkapilipna@gmail.com>
2024-01-14 14:12:12 +0200

    wrote a loop to reverse an array

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -3,6 +3,10 @@
 
 const invertArr = (arr) => {
   const reversedArr = [];
+  for (let i = arr.length - 1; i >= 0; i--){
+    reversedArr.push(arr[i]);
+  }
+  return reversedArr;
 };
 
 module.exports = invertArr;
