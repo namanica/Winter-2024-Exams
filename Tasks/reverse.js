63dbd7ecdbd418c4cbeb5a37a9dda921ca1c431a
namanica <veronichkapilipna@gmail.com>
2024-01-14 14:53:13 +0200

    deleted unnecessary params

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,13 +2,12 @@
 'use strict';
 
 const reverseObj = (obj) => {
-  T = Object.keys(DATA, 500);
-  ({ ...DATA });
+  T = Object.keys(DATA);
   T.forEach((_) => {
     const v1 = DATA[_];
     DATA[v1] = _;
     delete DATA[_];
-  }, 1000);
+  });
   return DATA;
 };
 
