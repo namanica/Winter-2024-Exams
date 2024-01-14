a288017d29615aaeae6b7a0b50c020543220b839
namanica <veronichkapilipna@gmail.com>
2024-01-14 16:44:49 +0200

    created array with filtered values

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,20 +2,7 @@
 'use strict';
 
 const sumArrNumbers = (arr) => {
-  sum = [0];
-  let k = 5;
-  for (i of s) {
-    let t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
-      }
-    }
-  }
-  sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
+  const arrNumbers = arr.filter((i) => typeof i === 'number');
+};
 
 module.exports = sumArrNumbers;
